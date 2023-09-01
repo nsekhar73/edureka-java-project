be58e130b886be5c698e5ac9b4bb14b10e75777c
bhasker-manikyala <28338138+bhasker-manikyala@users.noreply.github.com>
2023-09-01 16:45:42 +0000

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,7 +2,7 @@
 
 class Simple {
     public static void main (String[] args) {
-     System.out.println("Welcome to Git's Live Training!");
+     System.out.println("Welcome to Jenkins's Live Training!");
     }
 
 }
